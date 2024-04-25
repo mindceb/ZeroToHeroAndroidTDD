d5d8ec0ab714225ec152fa49ced53f5f2b0c1962
Kvitchenko, Artjom <AKvitchenko@qulix.com>
2024-04-25 11:20:43 +0200

    Task/009 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,42 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
+
+    private lateinit var rootLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rootLayout = findViewById(R.id.rootLayout)
+        titleTextView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.removeButton)
+        button.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val removedView = rootLayout.childCount == 1
+        outState.putBoolean(VISIBILITY_KEY, removedView)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.getBoolean(VISIBILITY_KEY)) {
+            rootLayout.removeView(titleTextView)
+        }
+    }
+
+    companion object {
+        private const val VISIBILITY_KEY = "key"
     }
 }
